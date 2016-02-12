4a869b9fdde70b2cf9d8a77c575bc77a06429526
Bruno Tavares <connect+github@bltavares.com>
2016-02-12 11:01:11 -0200

    Renders index html

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,20 @@ extern crate hyper;
 use hyper::server::{Server, Request, Response};
 use std::str::FromStr;
 
-fn hello(req: Request, res: Response) {
-	res.send(b"hi");
-}
+const DEFAULT_PORT : u16 = 7000;
+const INDEX_HTML : &'static str = include_str!("index.html");
 
 fn parse_port_str(port: String) -> u16 {
 	u16::from_str(&port).expect("Couldn't convert PORT to a number")
 }
 
-const DEFAULT_PORT : u16 = 7000;
+fn index(req: Request, res: Response) {
+	res.send(&INDEX_HTML.as_bytes()).expect("Couldn't write INDEX");
+}
 
 fn main() {
 	let port : u16 = std::env::var("PORT").map(parse_port_str).unwrap_or(DEFAULT_PORT);
 
 	println!("starting server on 0.0.0.0:{}", port);
-	Server::http(("0.0.0.0", port)).unwrap().handle(hello).unwrap();
+	Server::http(("0.0.0.0", port)).unwrap().handle(index).unwrap();
 }
